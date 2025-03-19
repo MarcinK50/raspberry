13639bcd0ca7e12fbed7ac6b7c5231644749486f
Marcin <kowalickimarcin13@gmail.com>
2025-03-19 17:31:16 +0100

    zapisywanie danych do pliku

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from machine import Pin
+from machine import Pin, RTC
 import dht
 from time import sleep
 
 sensor = dht.DHT22(Pin(16))
+
+def SaveIniFile(filename, dictionary):
+    with open(filename, "a") as f:
+        for key in dictionary:
+            f.write(f'{dictionary["time"]};{dictionary["temp"]};{dictionary["hum"]}\n')
+            
+def LoadIniFile(filename):
+    dictionary = {}
+    with open(filename, "r") as f:
+        for s in f:
+            lst = s.strip().split(",")
+            dictionary[lst[0]] = lst[1]
+    return dictionary
 
 while True:
   try:
@@ -12,5 +25,11 @@ while True:
     hum = sensor.humidity()
     print('Temperatura: %3.1f C' %temp)
     print('Wilgotnosc: %3.1f %%' %hum)
+    print(f'{RTC().datetime()}\n')
+    data = {}
+    data["time"] = f"{RTC().datetime()[4]}:{RTC().datetime()[5]}:{RTC().datetime()[6]}"
+    data["temp"] = temp
+    data["hum"] = hum
+    SaveIniFile("/MyData.txt", data)
   except OSError as e:
     print('Failed to read sensor.')
