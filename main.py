88223be1235a7350d1f030c27c482fdd2d9fa42d
Marcin <kowalickimarcin13@gmail.com>
2025-03-19 18:01:08 +0100

    laczenie z wifi i aktualizacja czasu przez NTP

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
 from machine import Pin, RTC
 import dht
 from time import sleep
+import network
+import ntptime
+import secret
+
+ssid = secret.ssid
+password = secret.password
+
+# Init Wi-Fi Interface
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+
+# Connect to your network
+wlan.connect(ssid, password)
+
+# Wait for Wi-Fi connection
+connection_timeout = 10
+while connection_timeout > 0:
+    if wlan.status() >= 3:
+        break
+    connection_timeout -= 1
+    print('Laczenie.....')
+    sleep(1)
+
+# Check if connection is successful
+if wlan.status() != 3:
+    raise RuntimeError('Blad polaczenia')
+else:
+    print('Pomyslnie polaczono!')
+    network_info = wlan.ifconfig()
+    print('IP:', network_info[0])
+    ntptime.host = "tempus1.gum.gov.pl"
+    ntptime.settime()
 
 sensor = dht.DHT22(Pin(16))
 
@@ -27,9 +59,9 @@ while True:
     print('Wilgotnosc: %3.1f %%' %hum)
     print(f'{RTC().datetime()}\n')
     data = {}
-    data["time"] = f"{RTC().datetime()[4]}:{RTC().datetime()[5]}:{RTC().datetime()[6]}"
+    data["time"] = f"{RTC().datetime()[4]+1}:{RTC().datetime()[5]}:{RTC().datetime()[6]}" # TODO: godzina w RTC jest w UTC; trzeba przesunac zgodnie z strefa
     data["temp"] = temp
     data["hum"] = hum
-    SaveIniFile("/MyData.txt", data)
+    SaveIniFile("/dane.txt", data)
   except OSError as e:
     print('Failed to read sensor.')
