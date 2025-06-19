58610d5f1a933b353b8be852f2956372baeb5f11
Marcin <kowalickimarcin13@gmail.com>
2025-06-19 20:26:15 +0200

    poprawka importowania biblioteki od UART

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import utime, network, dht, mrequests as requests
-from machine import Pin
+from machine import Pin, UART
 from pms5003 import PMS5003
 import secret
 
@@ -18,7 +18,7 @@ data_led.value(0)
 
 sensor = dht.DHT22(Pin(16))
 pms5003 = PMS5003(
-    uart=machine.UART(1, tx=Pin(8), rx=Pin(9), baudrate=9600),
+    uart=UART(1, tx=Pin(8), rx=Pin(9), baudrate=9600),
     pin_enable=Pin(3),
     pin_reset=Pin(2),
     mode="active"
