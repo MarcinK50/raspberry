49e39bf611ab7f3dcabb60dfc008e405fcd9e5a7
Marcin <kowalickimarcin13@gmail.com>
2025-06-19 19:48:51 +0200

    dodanie przykladowego pliku secret i lib mrequests

---
+++ secret.example.py
@@ -0,0 +1,9 @@
+ssid = 'wifi-ssid'
+password = 'wifi-password'
+
+server_ip = '192.168.10.10'
+server_port = '9000' # default port - 9000
+
+# questdb http basic auth user & password
+questdb_user = 'admin' 
+questdb_password = 'admin'
